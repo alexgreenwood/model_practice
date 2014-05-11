b954705fe6318f7e3017f6d178ff7b7500177fe6
alex greenwood <alexander.greenwood@gmail.com>
2014-05-11 16:57:11 -0500

    cleaned up q2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   def question_1
     # What is the most recent movie on the list that the second actor appeared in?
 
- the_actor = Actor.second
+    the_actor = Actor.second
 
     the_actors_movies = []
 
@@ -27,24 +27,70 @@ class QuestionsController < ApplicationController
 
 
 
-    # @director_of_longest_movie = ???
+    @longest_movie = Movie.order("").max.duration
+
+
+
+    @director_of_longest_movie = @longest_movie
+
+
+
+
+
+
   end
 
   def question_3
     # Which director has the most movies on the list?
 
-    # Your Ruby goes here.
+    movie_counts = []
 
-    # @director_with_the_most_movies = ???
+    Director.all.each do |the_director|
+    number_of_movies_directed_by_the_director = the_director.movies.count
+
+    movie_counts.push(number_of_movies_directed_by_the_director)
+    end
+
+    # @most_number_of_movies_by_a_single_director = movie_counts.max
+
+    # @director_with_the_most_movies =  @most_number_of_movies_by_a_single_director
+
+
+
+ most_recent_movie = { :year => 0, :name => "" }
+
+    the_directors_movies.each do |the_director|
+      if the_movie.year.to_i > most_recent_movie[:year]
+        most_recent_movie = { :year => the_movie.year.to_i, :title => the_movie.title }
+      end
+    end
+
+    @most_recent_movie_for_second_actor = most_recent_movie[:title]
+
+
+    # @director_with_the_most_movies =
+
+
   end
 
   def question_4
     # Which actor has been in the most movies on the list?
     # (If there's a tie, any one of them is fine)
 
-    # Your Ruby goes here.
+  # movie_counts = []
 
-    # @actor_with_the_most_movies = ???
+  #   Actor.all.each do |the_actor|
+  #     number_of_movies_acted_in = the_actor.movies.count
+
+  #     # movie_counts.push(number_of_movies_directed_by_the_director)
+
+
+  # #   # @most_number_of_movies_by_a_single_actor = movie_counts.max
+
+
+
+
+  #   @actor_with_the_most_movies =
   end
 
   def question_5
